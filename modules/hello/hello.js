dc1cf8b259261acf6a3e10496a707a557ecb25a5
julio <julio@pnoni.com>
2020-12-12 23:17:55 +0900

    moduler modified...

--- modules/hello/hello.js
+++ modules/hello/hello.js
@@ -1,6 +1,6 @@
 module.exports.hello = async (name) => {
     console.log('call hello')
-    const { install } = importModule('/modules/index.js')
+    const { install } = importModule('/modules/moduler/moduler.js')
     console.log('after install moduler')
     const lodash = await install('lodash')
     console.log('after install lodash')
