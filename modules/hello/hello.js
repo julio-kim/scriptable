7440c609bdccb754304de2b60ff762e3af34ddc8
julio <julio@pnoni.com>
2020-12-12 23:21:39 +0900

    hello module modified...

--- modules/hello/hello.js
+++ modules/hello/hello.js
@@ -1,10 +1,10 @@
-module.exports.hello = async (name) => {
-    console.log('call hello')
-    const { install } = importModule('/modules/moduler/moduler.js')
-    console.log('after install moduler')
-    const lodash = await install('lodash')
-    console.log('after install lodash')
-    
+console.log('call hello')
+const { install } = importModule('/modules/moduler/moduler.js')
+console.log('after install moduler')
+const lodash = await install('lodash')
+console.log('after install lodash')
+
+module.exports.hello = async (name) => {  
     let capName = lodash.capitalize(name)
     return `Hello ${capName}`
 }
