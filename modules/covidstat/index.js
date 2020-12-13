0b1bbf98f7c973520ce7cb228d555940fb86a324
julio <julio@pnoni.com>
2020-12-13 12:21:14 +0900

    covidstat initial commit

--- modules/covidstat/index.js
+++ modules/covidstat/index.js
@@ -3,7 +3,7 @@ const _init = function () {
     let txt = this.widget.addText('CovidStat')
     txt.centerAlignText()
 }
-/*
+
 const _loadData = async function () {
     const source = 'http://ncov.mohw.go.kr'
     let webView = new WebView()
@@ -25,7 +25,6 @@ const _loadData = async function () {
         date: covid.date.replace(/\(|\)/g, '').split(',')[0]
     }
 }
-*/
 
 class CovidStat {
     constructor (options) {
@@ -35,35 +34,11 @@ class CovidStat {
         let _options = Object.assign({
 
         }, options)
-
-
-        // let covid = await _loadData()
-        // console.log(covid)
-
-        //_init.bind(this)()
     }
 
     async init () {
-        const source = 'http://ncov.mohw.go.kr'
-        let webView = new WebView()
-        await webView.loadURL(source)
-    
-        let covid = await webView.evaluateJavaScript(`
-            const baseSelector = 'div.mainlive_container div.liveboard_layout '
-            let date = document.querySelector(baseSelector + 'h2 span.livedate').innerText
-            let domestic = document.querySelector(baseSelector + 'div.liveNum_today_new ul li:nth-child(1) span.data').innerText
-            let overseas = document.querySelector(baseSelector + 'div.liveNum_today_new ul li:nth-child(2) span.data').innerText
-            
-            completion({date, count: {
-                domestic: domestic.replace(",", ""), overseas
-            }, wpsData: WPS_data })
-        `, true)
-    
-        let count = parseInt(covid.count.domestic) + parseInt(covid.count.overseas)
-        let date = covid.date.replace(/\(|\)/g, '').split(',')[0]
-
-        console.log('count: ' + count)
-        console.log('date: ' + date)
+        this.covid = await _loadData()
+        _init.bind(this)()
     }
 
     present () {
