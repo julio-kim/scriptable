7903fb6d75fb0ad1d67a86de514b9dcff687a694
julio <julio@pnoni.com>
2020-12-13 12:36:22 +0900

    covidstat initial commit

--- modules/covidstat/index.js
+++ modules/covidstat/index.js
@@ -1,10 +1,4 @@
-const _init = function () {
-    
-    let txt = this.widget.addText('CovidStat')
-    txt.centerAlignText()
-}
-
-const _loadData = async function () {
+const _loadData = async () => {
     const source = 'http://ncov.mohw.go.kr'
     let webView = new WebView()
     await webView.loadURL(source)
@@ -26,13 +20,52 @@ const _loadData = async function () {
     }
 }
 
+const _getIconSize = () => Device.isPhone() ? new Size(12, 12) : new Size(16, 16)
+
+const _getLevelColor = (count) => {
+    if (count >= 500) return '#222831'
+    else if (count < 500 && count >= 300) return '#dc143c'
+    else if (count < 300 && count >= 100) return '#f05454'
+    else return '#0099ff'
+}
+
+const _getIcon = async (iconName, color = 'white') => {
+    let fm = FileManager.local()
+    let dir = fm.documentsDirectory()
+    let path = fm.joinPath(`${dir}`, `${iconName}.png`)
+    
+    if (fm.fileExists(path)) {
+        return fm.readImage(path)
+    } else {
+        let iconImage = await loadImage(`https://iconsdb.com/icons/download/${color}/${iconName}.png`)
+        fm.writeImage(path, iconImage)
+        return iconImage
+    }
+}
+
+const _init = function () {
+    
+    let txt = this.widget.addText('CovidStat')
+    txt.centerAlignText()
+}
+
 class CovidStat {
     constructor (options) {
         this.widget = new ListWidget()
         this.widget.setPadding(0, 0, 0, 0)
 
-        let _options = Object.assign({
+        if (options.size) {
+            let sizes = options.size.split['|']
+            options.titleSize = (sizes[0]) ? sizes[0] : undefined
+            options.countSize = (sizes[1]) ? sizes[1] : undefined
+            options.dateSize = (sizes[2]) ? sizes[2] : undefined
+        }
 
+        this.options = Object.assign({
+            refreshAfterSeconds: 30,
+            titleSize: '17:20',
+            countSize: '55:70',
+            dateSize: '15:15'
         }, options)
     }
 
