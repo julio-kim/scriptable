3ffdf589f87ec10fb005c77a63ebcb8522814684
julio <julio@pnoni.com>
2020-12-13 12:03:11 +0900

    covidstat initial commit

--- modules/covidstat/index.js
+++ modules/covidstat/index.js
@@ -1,15 +1,42 @@
 const _init = function () {
-    this.widget.setPadding(0, 0, 0, 0)
+    
     let txt = this.widget.addText('CovidStat')
     txt.centerAlignText()
+}
+
+const _loadData = async function () {
+    const source = 'http://ncov.mohw.go.kr'
+    let webView = new WebView()
+    await webView.loadURL(source)
+
+    let covid = await webView.evaluateJavaScript(`
+        const baseSelector = 'div.mainlive_container div.liveboard_layout '
+        let date = document.querySelector(baseSelector + 'h2 span.livedate').innerText
+        let domestic = document.querySelector(baseSelector + 'div.liveNum_today_new ul li:nth-child(1) span.data').innerText
+        let overseas = document.querySelector(baseSelector + 'div.liveNum_today_new ul li:nth-child(2) span.data').innerText
+        
+        completion({date, count: {
+            domestic: domestic.replace(",", ""), overseas
+        }, wpsData: WPS_data })
+    `, true)
+
+    return {
+        count: parseInt(covid.count.domestic) + parseInt(covid.count.overseas),
+        date: covid.date.replace(/\(|\)/g, '').split(',')[0]
+    }
 }
 
 class CovidStat {
     constructor (options) {
         this.widget = new ListWidget()
+        this.widget.setPadding(0, 0, 0, 0)
+
         let _options = Object.assign({
 
         }, options)
+
+        let covid = await _loadData()
+        console.log(covid)
 
         _init.bind(this)()
     }
