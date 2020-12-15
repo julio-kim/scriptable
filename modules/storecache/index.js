6a3f079203fd922af1cfe0f7fcf753f7979d508c
julio <julio@pnoni.com>
2020-12-15 11:45:34 +0900

    store added...

--- modules/storecache/index.js
+++
@@ -1,9 +0,0 @@
-const StoreBase = importModule('/modules/storebase')
-
-class CacheStore extends StoreBase {
-    constructor (storeName, options) {
-        super(FileManager.local().cacheDirectory(), storeName, options)
-    }
-}
-
-module.exports = CacheStore
--- modules/storetemp/index.js
+++
@@ -1,9 +0,0 @@
-const StoreBase = importModule('/modules/storebase')
-
-class TemporaryStore extends StoreBase {
-    constructor (storeName, options) {
-        super(FileManager.local().temporaryDirectory(), storeName, options)
-    }
-}
-
-module.exports = TemporaryStore
