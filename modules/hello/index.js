41ae2fa0c907e9d8945bab5b8a032955511283b0
julio <julio@pnoni.com>
2020-12-17 09:50:38 +0900

    hello modified...

--- modules/hello/index.js
+++ modules/hello/index.js
@@ -2,5 +2,10 @@ module.exports.hello = (name) => {
     const lodash = importModule('/modules/lodash')
     const moment = importModule('/modules/moment')
     let capName = lodash.capitalize(name)
-    return `Hello ${capName}!! (${moment().format('YYYY/MM/DD HH:mm:ss')})`
+    
+    let noti = new Notification()
+    noti.title = 'Scriptable Moduler'
+    noti.body = `Hello ${capName}!! (${moment().format('YYYY/MM/DD HH:mm:ss')})`
+    noti.sound = 'complete'
+    noti.schedule()
 }
