6235674afe671bd92368a03a12314a193905854f
julio <julio@pnoni.com>
2020-12-17 09:05:22 +0900

    moduler dependencies added...

--- modules/hello/index.js
+++ modules/hello/index.js
@@ -1,5 +1,6 @@
 module.exports.hello = (name) => {  
     const lodash = importModule('/modules/lodash')
+    const moment = importModule('/modules/moment')
     let capName = lodash.capitalize(name)
-    return `Hello ${capName}!!`
+    return `Hello ${capName}!! (${moment().format('YYYY/MM/DD HH:mm:ss')})`
 }
